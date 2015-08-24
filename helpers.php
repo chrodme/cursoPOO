67608867c25bb904f176a5504f4ac9ed40b28c38
CHRODME <chrodme@hotmail.com>
2015-08-24 16:53:46 -0500

    commit chamba

--- helpers.php
+++ helpers.php
@@ -21,5 +21,4 @@ function controller($name)
         header("HTTP/1.0 404 Not Found");
         exit("Pagina no encontrada");
     }
-
 }
--- index.php
+++ index.php
@@ -5,5 +5,20 @@
  */
 require 'config.php';
 require 'helpers.php';
+//Library
+require 'Library/Request.php';
+
 //Llamar al controlador indicado
-controller($_GET['url']);
+//controller($_GET['url']);
+
+if(empty($_GET['url']))
+{
+    $url = "";
+}
+else
+{
+    $url = $_GET['url'];
+}
+
+$request = new Request($url);
+var_dump($request->getController());
